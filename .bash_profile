0b0dc450f99ea1efe366c2aab94c63380e217c08
Maxwell Morgan <mjmor@quadmetrics.com>
2017-09-20 16:25:22 -0400

    update linker and bash profile

--- .bash_profile
+++ .bash_profile
@@ -4,4 +4,6 @@ export CLICOLOR=1
 # Set colors to match iTerm2 Terminal Colors
 export TERM=xterm-256color
 
-source ~/.bashrc
+if [ -e ~/.bashrc ]; then
+    . ~/.bashrc
+fi
--- bin/linker.sh
+++ bin/linker.sh
@@ -1,24 +1,16 @@
 # link vimrc
-if [ -f ~/.vimrc ]; then
-    rm -i ~/.vimrc
-fi
-ln -s ~/dot/.vimrc ~/.vimrc
+ln -fs ~/dot/.vimrc ~/.vimrc
 
 # link bashrc or bash_profile depending on OS
 SYSTYPE=`uname -s`
 if [[ $SYSTYPE =~ ^Darwin ]]
 then
-    rm ~/.bash_profile &> /dev/null
-    ln -s ~/dot/.bash_profile ~/.bash_profile
+    ln -fs ~/dot/.bash_aliases ~/.bash_aliases
+    ln -fs ~/dot/.bashrc ~/.bashrc
+    ln -fs ~/dot/.bash_profile ~/.bash_profile
     source ~/.bash_profile
 else
-    if [ -f ~/.bash_aliases ]; then
-        rm -i ~/.bash_aliases
-    fi
-    ln -s ~/dot/.bash_aliases ~/.bash_aliases
-    if [ -f ~/.bashrc ]; then
-        rm -i ~/.bashrc
-    fi
-    ln -s ~/dot/.bashrc ~/.bashrc
+    ln -fs ~/dot/.bash_aliases ~/.bash_aliases
+    ln -fs ~/dot/.bashrc ~/.bashrc
     source ~/.bashrc
 fi
