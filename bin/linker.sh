7fa31c2c965f38b80592e39dcfe63b2564004203
Maxwell Morgan <mjmor@umich.edu>
2017-09-20 15:54:26 -0400

    update linker to point to new repo

--- bin/linker.sh
+++ bin/linker.sh
@@ -1,29 +1,24 @@
-# link vimrc 
+# link vimrc
 if [ -f ~/.vimrc ]; then
-    rm -i ~/.vimrc 
+    rm -i ~/.vimrc
 fi
-ln -s ~/dot_repo/vimrc ~/.vimrc
+ln -s ~/dot/.vimrc ~/.vimrc
 
 # link bashrc or bash_profile depending on OS
 SYSTYPE=`uname -s`
 if [[ $SYSTYPE =~ ^Darwin ]]
-then 
+then
     rm ~/.bash_profile &> /dev/null
-    ln -s ~/dot_repo/bash_profile ~/.bash_profile
+    ln -s ~/dot/.bash_profile ~/.bash_profile
     source ~/.bash_profile
 else
     if [ -f ~/.bash_aliases ]; then
         rm -i ~/.bash_aliases
     fi
-    ln -s ~/dot_repo/bash_aliases ~/.bash_aliases
+    ln -s ~/dot/.bash_aliases ~/.bash_aliases
     if [ -f ~/.bashrc ]; then
         rm -i ~/.bashrc
     fi
-    ln -s ~/dot_repo/bashrc ~/.bashrc
+    ln -s ~/dot/.bashrc ~/.bashrc
     source ~/.bashrc
-    if [ -f ~/.ssh/config ]; then
-        rm -i ~/.ssh/config
-    fi
-    ln -fs ~/dot_repo/ssh/config ~/.ssh/config
-    chmod 600 ~/dot_repo/ssh/config
 fi
