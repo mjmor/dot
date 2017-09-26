ac8beba4f91ab9668f5e3875f6e8d52cd4ae8bd5
Maxwell Morgan <mjmor@quadmetrics.com>
2017-09-26 16:47:37 -0400

    update linker with bash completion

--- bin/linker.sh
+++ bin/linker.sh
@@ -5,23 +5,30 @@ ln -fs ~/dot/.always_forget.txt ~/.always_forget.txt
 
 # link bashrc or bash_profile depending on OS
 SYSTYPE=`uname -s`
-if [[ $SYSTYPE =~ ^Darwin ]]
-then
+if [[ $SYSTYPE =~ ^Darwin ]]; then
     ln -fs ~/dot/.bash_aliases ~/.bash_aliases
     ln -fs ~/dot/.bashrc ~/.bashrc
     ln -fs ~/dot/.bash_profile ~/.bash_profile
-    source ~/.bash_profile
 else
     ln -fs ~/dot/.bash_aliases ~/.bash_aliases
     ln -fs ~/dot/.bashrc ~/.bashrc
-    source ~/.bashrc
 fi
 
 # link bash completion scripts
 ln -fs ~/dot/.bash_completion ~/.bash_completion
+
+if [ ! -d ~/.bash_completion.d/ ]; then
+    mkdir -p ~/.bash_completion.d/
+fi
 
 for bcfile in ~/dot/.bash_completion.d/* ; do
     echo $bcfile
     echo $(basename ${bcfile})
     ln -fs $bcfile ~/.bash_completion.d/$(basename ${bcfile})
 done
+
+if [[ $SYSTYPE =~ ^Darwin ]]; then
+    source ~/.bash_profile
+else
+    source ~/.bashrc
+fi
